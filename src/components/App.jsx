f7c3c849cdf66b91774360240f88968795415167
pullarao99 <122142756+pullarao99@users.noreply.github.com>
2023-02-17 10:48:29 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Heading from "./Heading";
+import Note from "./Note";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+const react = require("react");
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Dev Town</h1>
+      <p>A place to Learn and Grow on you own peace</p>
+    </div>
+  );
+}
+export default Note;
